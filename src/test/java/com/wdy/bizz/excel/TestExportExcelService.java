35f7a6c0159ba4f238a0a2b7987671240d9a5724
wangguochao <1153540212@qq.com>
2019-05-05 16:59:55 +0800

    第一次版本提交

--- src/test/java/com/wdy/bizz/excel/TestExportExcelService.java
+++ src/test/java/com/wdy/bizz/excel/TestExportExcelService.java
@@ -1,11 +1,19 @@
 package com.wdy.bizz.excel;
 
+import cn.hutool.core.collection.CollectionUtil;
+import com.jfinal.plugin.activerecord.Record;
+import com.wdy.biz.dictionary.service.DictionaryService;
 import com.wdy.biz.excel.service.ExportExcelService;
 import com.wdy.bizz.TestWdyConfig;
 import com.wdy.message.OutMessage;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.wdy.config.constant.CommonConstant.FONE;
+import static com.wdy.config.constant.CommonConstant.ONE;
 
 /**
  * @author wgch
@@ -24,5 +32,31 @@ public class TestExportExcelService extends TestWdyConfig {
         System.out.println(message);
     }
 
+    /**
+     * 测试职务层次字典表
+     */
+    @Test
+    public void testZb09() {
+        DictionaryService dictionaryService = new DictionaryService();
+        // 父级code集合
+        List<String> A0221List = new ArrayList<>();
+        // 父级名称 集合
+        List<String> nameList = new ArrayList<>();
+        List<Record> zb091 = dictionaryService.getDictionaryList("ZB09");
+        for (Record record : zb091) {
+            String sub_code_value = record.getStr("SUB_CODE_VALUE");
+            String code_value = record.getStr("CODE_VALUE");
+            String code_name = record.getStr("CODE_NAME");
+            if (!sub_code_value.equals(ONE) && !sub_code_value.equals(FONE)
+                    && !CollectionUtil.contains(A0221List, sub_code_value)) {
+                A0221List.add(sub_code_value);
+            }
+            if ((sub_code_value.equals(ONE) || sub_code_value.equals(FONE) || sub_code_value.equals("7")) && !code_value.equals(ONE)) {
+                nameList.add(code_name);
+            }
+        }
+        System.out.println(A0221List);
+        System.out.println(nameList);
+    }
 
 }
