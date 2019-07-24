7e56b22953225f80d814ce1d97cba35cdf72d4c8
wangguochao <1153540212@qq.com>
2019-07-24 11:12:11 +0800

    第一次版本提交

--- src/main/java/com/wdy/utils/MD5Util.java
+++ src/main/java/com/wdy/utils/MD5Util.java
@@ -1,21 +1,30 @@
 package com.wdy.utils;
 
+import cn.hutool.crypto.digest.DigestUtil;
+
 import java.security.MessageDigest;
 
 public class MD5Util {
     private static final String[] hexDigits = {"0", "1", "2", "3", "4", "5",
             "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
 
-    public static String MD5Encode(String origin, String charsetname) {
+    /**
+     * MD5 加密
+     *
+     * @param origin      待加密字符串
+     * @param charsetName 字符集名称
+     * @return
+     */
+    public static String MD5Encode(String origin, String charsetName) {
         String resultString = null;
         try {
             resultString = new String(origin);
             MessageDigest md = MessageDigest.getInstance("MD5");
-            if (charsetname == null || "".equals(charsetname))
+            if (charsetName == null || "".equals(charsetName))
                 resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
             else
-                resultString = byteArrayToHexString(md.digest(resultString.getBytes(charsetname)));
-        } catch (Exception exception) {
+                resultString = byteArrayToHexString(md.digest(resultString.getBytes(charsetName)));
+        } catch (Exception e) {
         }
         return resultString;
     }
@@ -37,4 +46,11 @@ public class MD5Util {
         return hexDigits[d1] + hexDigits[d2];
     }
 
+    public static void main(String[] args) {
+        String pwd = MD5Encode("123456", "");
+        System.out.println(pwd);
+
+        String s = DigestUtil.md5Hex("123456");
+        System.out.println(s);
+    }
 }
