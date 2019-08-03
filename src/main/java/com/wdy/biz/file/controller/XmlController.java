0387be12ccc8cad37fbbd68265eb28b02e4d33ff
Bizzaro <1153540212@qq.com>
2019-08-03 22:16:15 +0800

    第一次版本提交

--- src/main/java/com/wdy/biz/file/controller/XmlController.java
+++ src/main/java/com/wdy/biz/file/controller/XmlController.java
@@ -10,16 +10,33 @@ import java.util.List;
 
 /**
  * @author wgch
- * @Description
+ * @Description xml 操作
  * @date 2019/7/26 12:16
  */
 public class XmlController {
     public static final String a01Path = "D:\\wdy\\wdy_jfinal_demo\\src\\main\\webapp\\WEB-INF\\view\\book\\A01.xml";
 
     public static void main(String[] args) {
-        getA01XmlData(a01Path);
+//        getA01XmlData(a01Path);
 
-        String value = "<xml><row \"A0000\"=\"XZ &lt;\"/> </xml>";
+        escape();
+        unescape();
+    }
+
+    /**
+     * xml 字符串转义
+     */
+    private static void escape() {
+        String value = "\"<br />\"";
+        String s = org.apache.commons.lang3.StringEscapeUtils.escapeXml10(value);
+        System.out.println(s);
+    }
+
+    /**
+     * xml 反转义
+     */
+    private static void unescape() {
+        String value = "<xml><row \"A0000\"=\"现在 &lt;\"/> </xml>";
         String s = StringEscapeUtils.unescapeXml(value);
         System.out.println(s);
     }
