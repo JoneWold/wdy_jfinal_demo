2d9e07c60427576b05c30f428b7202f8f0ba97bb
wangguochao <1153540212@qq.com>
2019-11-13 18:42:24 +0800

    第一次测试版本提交

---
+++ pgsql/devdb_dict_20190813.sql
@@ -0,0 +1,46 @@
+	-- 政治面貌
+	SELECT * from code_value where "CODE_TYPE"='GB4762'  
+	-- 民族
+	SELECT * from code_value where "CODE_TYPE"='GB3304'
+	-- 性别
+	SELECT * from code_value where "CODE_TYPE"='GB2261'
+	-- 学历
+	SELECT * from code_value where "CODE_TYPE"='ZB64'
+	-- 学位
+	SELECT * from code_value where "CODE_TYPE"='GB6864'
+	-- 专业
+	SELECT * from code_value where "CODE_TYPE"='GB16835'
+
+-- 管理类别
+SELECT * from code_value where "CODE_TYPE"='ZB130'
+-- 人员类别
+SELECT * from code_value where "CODE_TYPE"='ZB125'
+
+-- 职务层次
+select * from code_value where "CODE_TYPE"='ZB09' 
+-- 职级
+select * from code_value where "CODE_TYPE"='ZB148'
+
+
+
+-- 岗位级别
+SELECT * from code_value where "CODE_TYPE"='ZZB11'
+-- 单位隶属关系
+SELECT *from code_value where  "CODE_TYPE"='ZB87'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
