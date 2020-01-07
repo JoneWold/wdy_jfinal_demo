26b5832db11452ff8b5daf70cc50a08c42436f55
wgch <1153540212@qq.com>
2020-01-07 18:07:37 +0800

    第一次测试版本提交

--- src/main/java/com/wdy/biz/excel/service/ExportMemUnitService.java
+++ src/main/java/com/wdy/biz/excel/service/ExportMemUnitService.java
@@ -12,6 +12,7 @@ import com.wdy.message.OutMessage;
 import com.wdy.message.Status;
 import com.wdy.utils.XmlZipFileUtil;
 import org.apache.poi.openxml4j.util.ZipSecureFile;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -31,6 +32,11 @@ import java.util.stream.Collectors;
  */
 public class ExportMemUnitService {
 
+    /**
+     * Excel 样式
+     */
+    private static CellStyle cellStyle;
+    private static Workbook workbook;
     private ExportMemUnitDao memUnitDao = Aop.get(ExportMemUnitDao.class);
 
     public OutMessage exportMemUnit(Long orgId, String orgCode) throws Exception {
@@ -95,7 +101,11 @@ public class ExportMemUnitService {
     private void setUnitInfo(XSSFWorkbook wb, UnitInstitution unit) {
         // 获取第一个sheet页
         XSSFSheet orgSheet = wb.getSheetAt(0);
-
+        orgSheet.getRow(3).getCell(6).setCellValue(unit.getUnitAttributesName());
+        orgSheet.getRow(4).getCell(6).setCellValue(unit.getUnitCountLevelName());
+        orgSheet.getRow(5).getCell(6).setCellValue(unit.getUnitTypeName());
+        orgSheet.getRow(6).getCell(6).setCellValue(unit.getContact());
+        orgSheet.getRow(7).getCell(6).setCellValue(unit.getPhone());
     }
 
     /**
@@ -103,7 +113,18 @@ public class ExportMemUnitService {
      */
     private void setUnitList(XSSFWorkbook wb, UnitInstitution unit) {
         XSSFSheet sheet = wb.getSheetAt(1);
-
+        Row row = this.getRow(sheet, 2);
+        setCell(row, 0, unit.getName());
+        setCell(row, 1, unit.getTermInstitutionName());
+        setCell(row, 2, unit.getTermTargetInstitutionName());
+        setCell(row, 3, unit.getPreYearAssessName());
+        setCell(row, 4, unit.getInYearAssessName());
+        setCell(row, 5, String.valueOf(unit.getPreMainLeader()));
+        setCell(row, 6, String.valueOf(unit.getPreMinorLeader()));
+        setCell(row, 7, String.valueOf(unit.getPreOtherLeader()));
+        setCell(row, 8, String.valueOf(unit.getMainLeader()));
+        setCell(row, 9, String.valueOf(unit.getMinorLeader()));
+        setCell(row, 10, String.valueOf(unit.getOtherLeader()));
 
     }
 
@@ -129,6 +150,51 @@ public class ExportMemUnitService {
     private void setUnitCFQKemInfo(XSSFWorkbook wb, List<MemUnitInstitution> memList) {
         XSSFSheet sheet = wb.getSheetAt(4);
 
+    }
+
+
+    /***
+     * 获取第几行
+     * @param sheet 单元格
+     * @param index 行数
+     */
+    public Row getRow(Sheet sheet, Integer index) {
+        Row row = sheet.getRow(index);
+        if (row == null) {
+            row = sheet.createRow(index);
+        }
+        return row;
+    }
+
+    /**
+     * 设置单元格值
+     *
+     * @param row   行
+     * @param index 列号
+     * @param value 数据
+     */
+    private synchronized static void setCell(Row row, Integer index, String value) {
+        Cell cell = row.getCell(index);
+        if (cell == null) {
+            cell = row.getCell(index, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+        }
+
+        if (workbook == null || !workbook.equals(row.getSheet().getWorkbook())) {
+            workbook = row.getSheet().getWorkbook();
+            cellStyle = null;
+        }
+        if (cellStyle == null) {
+            CellStyle cellStyle = workbook.createCellStyle();
+            cellStyle.setBorderLeft(BorderStyle.THIN);
+            cellStyle.setBorderRight(BorderStyle.THIN);
+            cellStyle.setBorderBottom(BorderStyle.THIN);
+            cellStyle.setBorderTop(BorderStyle.THIN);
+            DataFormat dataFormat = workbook.createDataFormat();
+            cellStyle.setDataFormat(dataFormat.getFormat("@"));
+        }
+
+        cell.setCellStyle(cellStyle);
+        cell.setCellValue(value);
     }
 
 
